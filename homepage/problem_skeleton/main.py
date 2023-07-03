5af7323e763c1711f08b8cf6d193e1793bea8f7d
Zahin <alvii147@gmail.com>
2023-07-03 12:17:56 -0400

    improved problem skeleton gif

--- homepage/problem_skeleton/main.py
+++ homepage/problem_skeleton/main.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import imageio.v2 as imageio
 
@@ -7,10 +8,7 @@ SCRIPT_PATH = Path(__file__).parent
 if __name__ == '__main__':
     imageio.mimsave(
         SCRIPT_PATH / 'problem_skeleton.gif',
-        [
-            imageio.imread(SCRIPT_PATH / 'problem_skeleton_no_cursor.png'),
-            imageio.imread(SCRIPT_PATH / 'problem_skeleton_with_cursor.png'),
-        ],
+        [imageio.imread(SCRIPT_PATH / filepath) for filepath in os.listdir(SCRIPT_PATH) if filepath.endswith('.png')],
         format='GIF',
         loop=0,
         duration=500,
